49df4341dad14e00a144b451d0c95b823c0d8a93
Mateusz Warzyński <mathew.warzynski@gmail.com>
2018-12-04 22:16:02 +0100

    Initially add files

--- heat-simulation.asm
+++ heat-simulation.asm
@@ -1,9 +1,49 @@
+extern malloc
+
 section .text
 	global start
 	global step
 
+section .data
+    board:  DQ 0
+    boardd: DQ 0
+	width:  DD 0
+	height: DD 0
+    heater: DQ 0
+    cooler: DQ 0
+    proportion: DD 0
+
+; Arguments:
+;   rdi - width of the matrix
+;   rsi - height of the matrix
+;   rdx - initial value for the matrix fields
+;   rcx - value for the heat engine
+;   r8  - value for the cool engine
+;   r9  - value for the proportion of heat movement
 start:
-	ret
+    ; Set up variables.
+	mov [width], edi
+	mov [height], esi
+    mov [board], rdx
+    mov [heater], rcx
+    mov [cooler], r8
+    mov [proportion], r9d
+    ; Allocate memory for processing the board.
+    ; Additional memory will be useful for keeping heat changes.
+    mov eax, [width]
+    mul DWORD[height] ; rax = [width] * [height]
+    mov rsi, 0x4
+    mul rsi           ; rax *= 4;
+    mov rdx, rax
+    call malloc       ; malloc([width] * [height] * 4)
+    test rax, rax
+    jz start_fail     ; If no error, return.
+    ret
+start_fail:
+    mov rax, 0 ; exit syscall
+    mov rbx, 1 ; exit code
+    syscall
 
 step:
+	; Use SSE (for SSE3?).
 	ret
--- heat-simulation.c
+++ heat-simulation.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // start prepares simulation, initializes memory.
 void start(
@@ -14,5 +15,32 @@ void start(
 void step();
 
 int main() {
+    int width = 10;
+    int height = 10;
+	float *M, *G, *C;
+    float weight = 0.5;
+
+    M = malloc(sizeof(float)*width*height);
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            M[i*width + j] = 4;
+        }
+    }
+
+    G = malloc(sizeof(float)*width*2);
+    for (int i = 0; i < width; i++) {
+        G[i] = 8;
+        G[i + width] = 8;
+    }
+    C = malloc(sizeof(float)*height*2);
+    for (int i = 0; i < height; i++) {
+        C[i] = 1;
+        C[i + height] = 1;
+    }
+
+	start(width, height, M, G, C, weight);
+	step();
+
+    free(M); free(G); free(C);
 	return 0;
 }
